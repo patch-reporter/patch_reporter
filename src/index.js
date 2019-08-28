13e062dcd91d63d5025a281f1ad89b242e1a9073
bestseob93 <bestseob93@gmail.com>
2019-08-28 14:17:35 +0900

    ga 스크립트 수정

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-sequences */
+/* eslint-disable no-unused-expressions */
 import showdown from 'showdown';
 import { fetchReleaseNotes } from './service/github';
 import { qs, qsa, getCurrentDate, $on, loadElements, toggleLoading, $delegate, filterObject } from './utils/helper';
@@ -16,18 +19,29 @@ chrome.storage.sync.get('defaultnewtab', function(storage) {
     }
 });
 $on(window, 'load', function() {
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-86164198-1']);
-    _gaq.push(['_trackPageview']);
+    // Standard Google Universal Analytics code
 
-    (function() {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = 'https://ssl.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        (i[r] =
+            i[r] ||
+            function() {
+                (i[r].q = i[r].q || []).push(arguments);
+            }),
+            (i[r].l = 1 * new Date());
+        (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga'); // Note: https protocol here
+
+    ga('create', 'UA-86164198-1', 'auto');
+
+    ga('set', 'checkProtocolTask', function() {}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+
+    ga('require', 'displayfeatures');
+
+    ga('send', 'pageview', '/index.html');
 
     const settingBtnWrap = qs('.setting-btn-wrapper');
     const optionUrl = chrome.runtime.getURL('option.html');
